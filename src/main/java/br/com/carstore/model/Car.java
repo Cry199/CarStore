758576292b62e72fdcba6b0a5bd00228ac788fb7
caua.aribeiro <caualeu2016@gmail.com>
2023-10-20 20:24:14 -0300

    LAB 4 - Criando o método para DELETE

--- src/main/java/br/com/carstore/model/Car.java
+++ src/main/java/br/com/carstore/model/Car.java
@@ -2,14 +2,21 @@ package br.com.carstore.model;
 
 public class Car
 {
+    private String id;
+
     private String name;
 
     public Car() {}
 
     public Car(String name) { this.name = name; }
 
+    public Car(String id, String name) { this.id = id; this.name = name;}
 
-    public String getName() {return name;}
+    public String getId() { return id; }
 
-    public void setName(String name) { this.name = name;}
+
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 }
